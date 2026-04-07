b0052d93bc878a9a30d2fcbef8a9d3bc1709a2e5
Anu <anunishad279@gmail.com>
2026-04-07 20:35:34 +0530

    Update database password

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hospital_management";
     private static final String USER = "root";     
-    private static final String PASS = "Root";  
+    private static final String PASS = "myPassword";  
 
     public static Connection getConnection() {
         try {
